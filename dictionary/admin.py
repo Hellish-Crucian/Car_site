3577848e428bdacf1ebcca82cd6b8c5df97ae5f8
Crucian <daniilnikitin020405@gmail.com>
2020-10-11 11:25:45 +0600

    init

---
+++ dictionary/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from dictionary import models
+
+
+@admin.register(models.State)
+class StateAdmin(admin.ModelAdmin):
+	pass
+
+@admin.register(models.Mark)
+class MarkAdmin(admin.ModelAdmin):
+	pass
+	
+@admin.register(models.Model)
+class ModelAdmin(admin.ModelAdmin):
+	pass
+
---
+++ dictionary/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class State(models.Model):
+	name = models.CharField(max_length=32)
+	
+	def __str__(self):
+		return self.name
+
+
+class Mark(models.Model):
+	name = models.CharField(max_length=100)
+	
+	def __str__(self):
+		return self.name
+
+
+class Model(models.Model):
+	name = models.CharField(max_length=100)
+	model = models.ForeignKey('Mark', on_delete=models.CASCADE)
+	
+	def __str__(self):
+		return self.name
