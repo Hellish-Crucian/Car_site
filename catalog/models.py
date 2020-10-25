ebb16032b00ee5de2290b78a63f5e23b0aef84ae
Crucian <daniilnikitin020405@gmail.com>
2020-10-25 11:11:25 +0600

    ...

--- catalog/models.py
+++ catalog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Car(models.Model):
     name = models.CharField(max_length=100)
-	mark = models.ForeignKey('dictionary.Mark', on_delete=models.CASCADE)
+    mark = models.ForeignKey('dictionary.Mark', on_delete=models.CASCADE)
     model = models.ForeignKey('dictionary.Model', on_delete=models.CASCADE)
     state = models.ForeignKey('dictionary.State', on_delete=models.CASCADE)
     color = models.ForeignKey('dictionary.Color', on_delete=models.CASCADE)
