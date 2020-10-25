d6ac0b8d61254e671b6977094402a00a4aaa2ab8
Crucian <daniilnikitin020405@gmail.com>
2020-10-25 11:02:25 +0600

    Make classs Car

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+	model = models.ForeignKey('dictionary.Mark', on_delete=models.CASCADE)
+    model = models.ForeignKey('dictionary.Model', on_delete=models.CASCADE)
+    model = models.ForeignKey('dictionary.State', on_delete=models.CASCADE)
+    model = models.ForeignKey('dictionary.Color', on_delete=models.CASCADE)
+    prise = models.IntegerField()
+    year = models.IntegerField()
